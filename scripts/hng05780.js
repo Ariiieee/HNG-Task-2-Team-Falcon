9fd6330a97a21eff267fcf1710109938e358dcef
Oluwafunmilayo Dada <45949325+Ariiieee@users.noreply.github.com>
2020-06-03 17:05:34 +0100

    Create hng05780.js
    A brief Introduction using Javascript

---
+++ scripts/hng05780.js
@@ -0,0 +1,8 @@
+let fullName = 'Oluwafunmilayo Dada';
+const email = 'ariiiearie@gmail.com';
+const hngId = '05780';
+const language = 'Javascript';
+console.log('Hello World, ' + ' This is ' + fullName + 
+            ', Email: ' + email + 
+            ' with HNGi7 ID ' + hngId + ' using ' + 
+            language + ' for Stage Two Task.' ); 
